30216fe7d917e4d836d8ecf67ba56e9a87eaf39a
Andrew <nerdyfoster@gmail.com>
2014-04-16 18:47:01 -0400

    Andrew's Changes
    Include the test files for the view contact page. Page is rough, but is
    being modified.

---
+++ ContactBookProject - Foster's Files/home.php
@@ -0,0 +1,117 @@
+<?php
+/*
+!!pseudo code!!
+	if (login != true)
+	{
+		header("location:login.php");	
+	}
+*/
+
+	/*this needs to be changed to SQL commands, but for basic usage, this should work*/
+	
+	$file_handle = fopen("sample.txt", "r");
+	
+		$fname = fgets($file_handle);
+		$mi = fgets($file_handle);
+		$lname = fgets($file_handle);
+		$address = fgets($file_handle);
+		$phoneNumber = fgets($file_handle);
+		$phoneNum2 = fgets($file_handle);
+		$email1 = fgets($file_handle);
+		/*
+   		echo "$dateString <br>";
+   		echo "$oldBalance <br>";
+   		echo "$balanceOld <br>";
+   		echo "$newBalance <br>";
+   		echo "$balanceNew <br>";
+   		echo "<br>";
+		*/
+
+	
+	$file_handle = fclose($file_handle);
+	
+?>
+<!DOCTYPE html PUBLIC>
+<head>
+<meta name="viewport" content="width=device-width", initial-scale="1">
+<link rel="stylesheet" href="CSS/NerdLair2.css" />
+<script type="text/javascript">
+function displayDate()
+{
+	
+	var d = new Date();
+	var x = document.getElementById("copyrighthere");
+	//document.write("Copyright &#169;");
+	//x.innerHTML=d.getFullYear();
+	
+	if (d == 2014)
+	{
+		document.write("Site &#169; Group FMH 2014.");
+	}
+	else if (d != 2014)
+	{
+		//document.write("Site &#169; McCallister, Hovemeyer, and Foster 2014 - ");
+		x.innerHTML="Site &#169; McCallister, Hovemeyer, and Foster 2014 - " + d.getFullYear();
+		//document.write("Site &#169; McCallister, Hovemeyer, and Foster 2014 - ");
+	}
+}
+</script>
+<!--This simply dynamically changes the title of the page to the first name-->
+<?php
+echo "<title>$fname $mi $lname</title>";
+?>
+<!--<title>Home</title>-->
+</head>
+
+<body>
+<h1>
+<center>
+<!--This displays the contacts name at the center and top of the page-->
+<?php
+echo $fname;
+echo $mi;
+echo $lname;
+?>
+</center></h1>
+<hr>
+<p id="boldstuff">
+Address:
+<?php
+echo $address;
+?>
+<br/>
+<hr>
+ <!--the different categories of numbers-->
+Phone numbers<br/>
+<?php
+
+/*
+Purpose: To check if there is a null secondary number, and if not, display it
+*/
+if ($phoneNum2 == 'null' || $phoneNum2 == "\n")
+{
+	echo "Primary: $phoneNumber";
+}
+else if ($phoneNum2 != 'null')
+{
+	echo "<br>";
+	echo "Phone number [secondary]: $phoneNum2";
+}
+?>
+<hr>
+Email addresses<br/>
+<?php
+/*I don't know if we want to do this or not, but make the emails go open in a mail client?*/
+echo "Primary <a href='mailto:SESSION.START.andrewmfoster@gmail.com'> $email1 </a>";
+?>
+<br/>
+<hr>
+<span id="copyrighthere">
+<script type="text/javascript">
+//calls the function to display the year
+displayDate();
+</script>
+<br/>
+</span>
+</body>
+</html>
---
+++ testMap.php
@@ -0,0 +1,5 @@
+<html>
+<body>
+<img border="0"  alt="" src="http://maps.googleapis.com/maps/api/staticmap?key=AIzaSyA-d9p8eSmHByP_0emwNUJzgSaYal9abA4&center=Berkeley,CA&zoom=14&size=400x400&markers=color:blue%7CBerkeley,CA&sensor=false" />
+</body>
+</html>
