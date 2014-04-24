cc266b4b66a53658e28b125aadc3ff0b9a387b81
Hovemeyer5 <Hovemeyer5@marshall.edu>
2014-04-24 09:19:44 -0400

    Add to Responsive and Updates to Edit Contact

--- editContact.php
+++ editContact.php
@@ -9,42 +9,86 @@
       else
       {
         ?>
-          <div class="header">
+          <div class="header editHeader">
 			<h3 class="text-muted pull-left">Add Contact</h3>
             <ul class="nav nav-pills pull-right">
-              <li><a href="#">Cancel</a></li>
+              <li><a href="<?=$_SERVER['PHP_SELF']?>">Cancel</a></li>
               <li><a id="save" href="#">Save</a></li>
             </ul> 
           </div>
     
-			<div class="row marketing">
+			<div class="row-fluid  contactImage">
+			    <div>
+				<div class="border imageBox">
+				    <div class="imageContainer">
+					<img src="i/dummy_image.jpg" />
+				    </div>
+				</div>
+				<input name="lname" type="file" class="form-control">
+			    </div>
+			</div>
+			<div class="row-fluid contactName">
+			    <div>
 				<form>
 					<div>
-					Image:
-					</div>
-					<div>
-					Fname:
-					MI:
-					Lname:
-					</div>
-					<div>
-					Phone:
-					</div>
-					<div>
-					Email:
-					</div>
-					<div>
-					Address:
-					</div>
-					<div>
-					URL:
+					    <div>
+						<span>Name:</span>
+						<button>+</button>
+					    </div>
+						<input name="fname" type="text" class="form-control" placeholder="First Name" required autofocus>
+						<input name="mi" type="text" class="form-control" placeholder="Middle Initial">
+						<input name="lname" type="text" class="form-control" placeholder="Last Name">
 					</div>
 				</form>
+			    </div>
+			</div>
+			<div class="row-fluid contactDetails">
+				<div>
+				    <form>
+					<div>
+					    <div>
+						<span>Phone:</span>
+						<button id="addPhone">+</button>
+					    </div>
+					    <div class="detailBox">
+						<input name="phone" type="text" class="form-control" placeholder="Phone">
+					    </div>
+					</div>
+					<form>
+					    <div>
+						<span>Address:</span>
+						<button id="addAddress">+</button>
+					    </div>
+					    <div class="detailBox">
+						<input name="street" type="text" class="form-control" placeholder="Street">
+						<input name="city" type="text" class="form-control" placeholder="City">
+						<input name="state" type="text" class="form-control" placeholder="State">
+						<input name="zipcode" type="text" class="form-control" placeholder="Zipcode">
+					    </div>
+					</form>
+					<div>
+					    <div>
+						<span>Email:</span>
+						<button id="addEmail">+</button>
+					    </div>
+					    <div class="detailBox">
+						<input name="email" type="text" class="form-control" placeholder="Email">
+					    </div>
+					</div>
+					
+					<div>
+					     <div>
+						<span>URL:</span>
+						<button id="addURL">+</button>
+					    </div>
+					    <div class="detailBox">
+						<input name="url" type="text" class="form-control" placeholder="URL">
+					    </div>
+					</div>
+				    </form>
+				</div>
 			 </div>
     
-          <div class="footer">
-            <p>&copy; Company 2014</p>
-          </div>
         <?
       }
 ?>
--- inc/header.php
+++ inc/header.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+
     <meta name="description" content="">
     <link rel="shortcut icon" href="../../assets/ico/favicon.ico">
 
@@ -12,6 +13,7 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="js/bootstrap.min.js"></script>
+    <link href="css/bootstrap-responsive.min.css" rel="stylesheet">
     
     <!-- Bootstrap core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
@@ -19,7 +21,7 @@
     <!-- Custom styles for this template -->
 	    <link href="css/jumbotron-narrow.css" rel="stylesheet">
 	    <link href="css/signin.css" rel="stylesheet">
-			
+	    <link href="css/edit.css" rel="stylesheet">	
   </head>
 
   <body>
