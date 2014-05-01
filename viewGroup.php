8519b9b0c6be3f7840804b8ebf35764f917a11de
Shukketsu1337 <Shukketsu1337@users.noreply.github.com>
2014-05-01 11:41:41 -0400

    Update viewGroup.php
    Added the map aspect and places markers on the map.

--- viewGroup.php
+++ viewGroup.php
@@ -10,40 +10,66 @@
       {
           //this is supposed to get group member information [question mark]
           //ask Rebecca and have her correct me as I'm more than likely doing something wrong
+          $theID = 1;
           $gname = $contacts->getGroupById($theID);
-          [g_id] => 1
-        $gname = [g_name] => TestGroup
-        [members] => Array
-        (
-            [0] => Array
-                (
-                    [c_id] => 1
-                    [c_name] => Miss L America
-                )
-
-            [1] => Array
-                (
-                    [c_id] => 2
-                    [c_name] => Rebecca R Hovemeyer
-                )
-
-        )
+		  echo "<pre>";
+		  print_r($gname);
+		  echo "</pre>";
         ?>
           <div class="header">
             <ul class="nav nav-pills pull-right">
               <li class="active"><a href="index.php">Home</a></li>
             </ul>
-            <h3 class="text-muted"><?=$gname?></h3>
+            <h3 class="text-muted"><?=$gname['g_name']?></h3>
           </div>
     
           <?php
-          /*
-          for (each member in the array)
+          echo $gname['g_name'] . "<br>";
+          foreach($gname['members'] as $member)
           {
-            echo "[c_name]";
+			  echo "<div><a href='viewContact.php?id=".$value[c_id]."'>".$value[$c_name]."</a>$member[c_name].</div>";
+            //echo $member[c_name]. "<br>";
           }
-          
-          */
+		  foreach ($gname['members'] as $member)
+		  {
+
+		  	$street = $member[address][a_street];
+        	$city = $member[address][a_city];
+        	$state = $member[address][a_state];
+        	$zip = $member[address][a_zip];
+			if($street != "")
+			{
+				$streetarray = explode(" ", $street);
+				
+				$newstreet = "";
+				foreach($streetarray as $key=>$val)
+				{
+					if($key > 0)
+					{
+						$newstreet += "+" . $val;
+					}
+					else
+					{
+						//val isn't set to anything
+						$newstreet = $val;
+					}
+					
+				}
+				echo $newstreet;
+				//debug purposes only
+				echo "<br/>";
+				
+				//for some reason, doing $mapstring += didn't work
+				$mapstring = $mapstring . "&markers=color:red%7C" . $newstreet ."+".$city."+".$state;
+		  	}
+		  }
+			//$mapAddress = $street."+".$city.",".$state;
+
+          //for ($k = 0; $k < $i; $k++)
+		  //echo "$addressArray[$k]";
+		  //time to do the map
+          echo "<img border='0'  alt='' src='http://maps.googleapis.com/maps/api/staticmap?key=AIzaSyA-d9p8eSmHByP_0emwNUJzgSaYal9abA4&center=1+John+Marshall+Drive+Huntington+WV&zoom=12
+&size=400x400&$mapstring&sensor=false,' />";
           ?>
     
           <div class="footer">
